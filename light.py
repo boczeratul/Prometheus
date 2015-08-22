389d0dc4b030b485562bc1358842ed0973fa3518
Hsuan Lee <boczeratul@gmail.com>
2015-08-22 20:43:02 +0800

    simple light on

---
+++ light.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 4
+
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.output(led, 1)
+
+time.sleep(5)
+
+GPIO.output(led, 0)
